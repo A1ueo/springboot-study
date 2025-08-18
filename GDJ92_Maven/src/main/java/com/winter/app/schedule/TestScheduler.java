dfc9adec5a0e2981b5fea5737408a47a936e07ad
SJ <ligerssj@naver.com>
2025-08-18 17:06:38 +0900

    테스트 스케줄러

---
+++ GDJ92_Maven/src/main/java/com/winter/app/schedule/TestScheduler.java
@@ -0,0 +1,32 @@
+package com.winter.app.schedule;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestScheduler {
+
+//	@Autowired
+//	private MemberMapper memberDAO;
+//	
+//	@Scheduled(fixedRateString = "2000", initialDelay = 1000)	// 무조건
+//	public void m1() throws Exception {
+//		System.out.println("================== m1");
+//		System.out.println(LocalTime.now());
+//		Thread.sleep(3000);
+//		System.out.println(LocalTime.now());
+//	}
+//	
+//	@Scheduled(fixedDelay = 2000, initialDelayString = "2000")	// 끝난 시간 기준
+//	public void m2() throws Exception {
+//		System.out.println("================== m2");
+//		System.out.println(LocalTime.now());
+//		Thread.sleep(3000);
+//		System.out.println(LocalTime.now());
+//	}
+//	
+//	@Scheduled(cron = "3 * * * * *")
+//	public void m3() throws Exception {
+//		System.out.println("================== m3");
+//		System.out.println(LocalTime.now());
+//	}
+}
