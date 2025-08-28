c6b154e3cdad5f9441c2f791460f312eec63200e
SJ <ligerssj@naver.com>
2025-08-28 11:53:05 +0900

    gradle

---
+++ GDJ92_Gradle/src/main/java/com/study/a1ueo/Gdj92GradleApplication.java
@@ -0,0 +1,13 @@
+package com.study.a1ueo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdj92GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdj92GradleApplication.class, args);
+	}
+
+}
