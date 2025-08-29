d21b1e0780329dd6cf65547329acbf880fd0ea75
SJ <ligerssj@naver.com>
2025-08-29 14:32:32 +0900

    Pageable 커스텀 메서드

--- GDJ92_Gradle/src/main/java/com/study/a1ueo/board/notice/NoticeRepository.java
+++ GDJ92_Gradle/src/main/java/com/study/a1ueo/board/notice/NoticeRepository.java
@@ -1,7 +1,11 @@
 package com.study.a1ueo.board.notice;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeRepository extends JpaRepository<NoticeVO, Long> {
 
+	List<NoticeVO> findByBoardTitleContaining(String keyword, Pageable pageable) throws Exception;
 }
