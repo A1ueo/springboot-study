a6dc96c857c1014a1155424b1b65b54c468a90dd
SJ <ligerssj@naver.com>
2025-08-29 17:03:57 +0900

    다대다 테이블

--- GDJ92_Gradle/src/main/java/com/study/a1ueo/member/MemberRoleVO.java
+++ GDJ92_Gradle/src/main/java/com/study/a1ueo/member/MemberRoleVO.java
@@ -1,12 +1,8 @@
 package com.study.a1ueo.member;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,20 +16,20 @@ import lombok.Setter;
 @IdClass(MemberRolePK.class)
 public class MemberRoleVO {
 
-	@EmbeddedId
-	private MemberRolePK memberRolePK;
+//	@EmbeddedId
+//	private MemberRolePK memberRolePK;
 	
-//	@Id
-//	private String username;
-//	@Id
-//	private Long roleNum;
+	@Id
+	private String username;
+	@Id
+	private Long roleNum;
 	
-	@JoinColumn(name = "username")
-	@ManyToOne
-	@JsonIgnore
-	MemberVO memberVO;
-	@JoinColumn(name = "roleNum")
-	@ManyToOne
-	@JsonIgnore
-	RoleVO roleVO;
+//	@JoinColumn(name = "username")
+//	@ManyToOne
+//	@JsonIgnore
+//	MemberVO memberVO;
+//	@JoinColumn(name = "roleNum")
+//	@ManyToOne
+//	@JsonIgnore
+//	RoleVO roleVO;
 }
