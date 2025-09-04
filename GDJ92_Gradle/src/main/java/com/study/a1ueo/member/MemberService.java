bae22786c0ec0f75df65e3c21391871bafa5223e
SJ <ligerssj@naver.com>
2025-09-04 18:04:53 +0900

    ..

--- GDJ92_Gradle/src/main/java/com/study/a1ueo/member/MemberService.java
+++ GDJ92_Gradle/src/main/java/com/study/a1ueo/member/MemberService.java
@@ -1,7 +1,14 @@
 package com.study.a1ueo.member;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class MemberService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
 
 }
